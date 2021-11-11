f3a477166d5053cd31142384c2226f18f76cce60
DanielaGC <danielagc676@gmail.com>
2021-11-10 22:08:00 -0300

    Update Users.ts

--- src/data/Users.ts
+++ src/data/Users.ts
@@ -1,8 +1,29 @@
 export const USERS_DISCORD = [
+  /*
+  Example of object
   {
     user_id: 'name_crowdin',
     discord_id: 'discord_id',
     mention: 'discord_mention',
     id: 'id_crowdin'
+  } 
+  */
+  {
+    user_id: 'danielagc_',
+    discord_id: '395788326835322882',
+    mention: '<@395788326835322882>',
+    id: '14009629'
+  },
+  {
+    user_id: 'azuresky',
+    discord_id: '630502777633636362',
+    mention: '<@630502777633636362>',
+    id: '14875698'
+  },
+  {
+    user_id: 'vincks__',
+    discord_id: '630502777633636362',
+    mention: '<@630502777633636362>',
+    id: '14544852'
   }
 ]
